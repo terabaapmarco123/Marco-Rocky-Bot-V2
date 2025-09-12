d14f68185f91c31511a463448dae93f974f6cdfb
terabaapmarco123 <terabaapaaya3344@gmail.com>
2025-09-12 10:08:11 +0530

    Update setnameall.js

--- Priyansh/commands/setnameall.js
+++ Priyansh/commands/setnameall.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "setall",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "",
   description: "Set nicknames all members in Group",
   commandCategory: "Box Chat",
   usages: "[name]",
